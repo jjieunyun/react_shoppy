56cb095914d19c4f0c72741ba2af62c672f0e2a0
jieun <lesson0186@gmail.com>
2023-01-16 21:54:58 +0900

    feat:firebase Auth구현

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { FiShoppingBag } from "react-icons/fi";
+import { BsFillPencilFill } from "react-icons/bs";
+import { login, logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
+
+export default function Navbar() {
+  const [user, setUser] = useState();
+
+  const handleLogin = () => {
+    login();
+  };
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    onUserStateChange(setUser);
+  }, []);
+
+  return (
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <nav className="flex items-center gap-4 font-semibold">
+        <Link to="/products">Products</Link>
+        <Link to="/carts">Carts</Link>
+        <Link to="/products/new" className="text-2xl">
+          <BsFillPencilFill />
+        </Link>
+
+        {user && <User user={user} />}
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
+      </nav>
+    </header>
+  );
+}
